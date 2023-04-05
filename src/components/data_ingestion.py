82f0b9f530c91e065841bf60af15ab605603579c
XaCourse <courses4xa@gmail.com>
2023-04-05 11:50:44 +0530

    model training

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -8,6 +8,8 @@ from sklearn.model_selection import train_test_split
 from src.exception import CustomException
 from src.logger import logging
 from src.components.data_transformation import DataTransformation
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
 
 @dataclass
 class DataIngestionConfig:
@@ -51,4 +53,9 @@ if __name__ == "__main__":
     train_data, test_data = obj.initiate_data_ingestion()
 
     data_transformation = DataTransformation()
-    data_transformation.inititate_data_transformation(train_data, test_data)
+    train_arr, test_arr,_=data_transformation.inititate_data_transformation(train_data, test_data)
+
+    logging.info("return data after successfully data transfromation")
+
+    model_trainer = ModelTrainer()
+    print(model_trainer.initiate_model_trainer(train_arr, test_arr))
