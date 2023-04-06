d3f805a8e2cbd228749e45da798668e6edb718cc
XaCourse <courses4xa@gmail.com>
2023-04-06 11:31:43 +0530

    predict pipline

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -1,23 +1,23 @@
-# train model
 import os
 import sys
 from dataclasses import dataclass
 
 from catboost import CatBoostRegressor
-from sklearn.ensemble import(
+from sklearn.ensemble import (
     AdaBoostRegressor,
     GradientBoostingRegressor,
-    RandomForestRegressor
+    RandomForestRegressor,
 )
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.tree import DecisionTreeRegressor
-from sklearn.metrics import r2_score
 from xgboost import XGBRegressor
 
 from src.exception import CustomException
 from src.logger import logging
-from src.utils import save_object, evaluate_models
+
+from src.utils import save_object,evaluate_models
 
 @dataclass
 class ModelTrainerConfig:
@@ -26,18 +26,17 @@ class ModelTrainerConfig:
 class ModelTrainer:
     def __init__(self):
         self.model_trainer_config=ModelTrainerConfig()
-    
-    
-    def initiate_model_trainer(self, train_array, test_array):
+
+
+    def initiate_model_trainer(self,train_array,test_array):
         try:
-            logging.info("Split train and test data")
-            X_train, y_train, X_test, y_test = (
-                train_array[:, :-1],
-                train_array[:, -1],
-                test_array[:, :-1],
+            logging.info("Split training and test input data")
+            X_train,y_train,X_test,y_test=(
+                train_array[:,:-1],
+                train_array[:,-1],
+                test_array[:,:-1],
                 test_array[:,-1]
             )
-
             models = {
                 "Random Forest": RandomForestRegressor(),
                 "Decision Tree": DecisionTreeRegressor(),
@@ -47,7 +46,6 @@ class ModelTrainer:
                 "CatBoosting Regressor": CatBoostRegressor(verbose=False),
                 "AdaBoost Regressor": AdaBoostRegressor(),
             }
-            
             params={
                 "Decision Tree": {
                     'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
@@ -86,32 +84,36 @@ class ModelTrainer:
                 
             }
 
-            model_report:dict=evaluate_models(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test, models=models, param=params)
+            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,
+                                             models=models,param=params)
+            
+            ## To get best model score from dict
+            best_model_score = max(sorted(model_report.values()))
 
-            #to get best model score
-            best_model_socre = max(sorted(model_report.values()))
+            ## To get best model name from dict
 
-            # to get best model name
             best_model_name = list(model_report.keys())[
-                list(model_report.values()).index(best_model_socre)
+                list(model_report.values()).index(best_model_score)
             ]
-
             best_model = models[best_model_name]
 
-            if best_model_socre<0.6:
+            if best_model_score<0.6:
                 raise CustomException("No best model found")
-            
-            logging.info("Best model found for training and testing data")
+            logging.info(f"Best found model on both training and testing dataset")
 
             save_object(
                 file_path=self.model_trainer_config.trained_model_file_path,
-                obj="best_model"
+                obj=best_model
             )
 
-            predicted = best_model.predict(X_test)
-            r2_score_result = r2_score(y_test, predicted)
+            predicted=best_model.predict(X_test)
 
-            return r2_score_result
-        
+            r2_square = r2_score(y_test, predicted)
+            return r2_square
+            
+
+
+
+            
         except Exception as e:
-            raise CustomException(e, sys)
+            raise CustomException(e,sys)
--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,7 @@
 import os
 import sys
 import dill
+import pickle
 
 from src.exception import CustomException
 from sklearn.metrics import r2_score
@@ -43,5 +44,12 @@ def evaluate_models(X_train, y_train, X_test, y_test, models,param):
         
         return report
 
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, 'rb') as file_obj:
+            return dill.load(file_obj)
     except Exception as e:
         raise CustomException(e, sys)
